aecc7c34f926926a98e37d69976ce068d4479fce
JonKelly <jonkelly@protonmail.com>
2022-11-16 15:15:22 -0400

    Betting buttons are showing for both players

--- BettingGame/Assets/Scripts/ChipStack.cs
+++ BettingGame/Assets/Scripts/ChipStack.cs
@@ -1,6 +1,30 @@
+using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class ChipStack : MonoBehaviour
+public class ChipStack : NetworkBehaviour
 {
-    
+    private MeshRenderer[] chipRenderers;
+
+    private void Awake()
+    {
+        chipRenderers = GetComponentsInChildren<MeshRenderer>();
+    }
+
+    public override void OnNetworkSpawn()
+    {
+        base.OnNetworkSpawn();
+
+        SetColors();
+    }
+
+    private void SetColors()
+    {
+        Color color = Random.ColorHSV();
+
+        foreach (var item in chipRenderers)
+        {
+            item.material.color = color;
+        }
+    }
 }
--- BettingGame/Assets/Scripts/GameManager.cs
+++ BettingGame/Assets/Scripts/GameManager.cs
@@ -1,12 +1,17 @@
+using System;
 using Unity.Netcode;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class GameManager : NetworkBehaviour
 {
     public static GameManager Instance { get; private set; }
 
     [SerializeField] private Transform _playerOnePosition;
     [SerializeField] private Transform _playerTwoPosition;
+
+    [SerializeField] private GameObject _bettingButtonsPanel;
+
+    private const int MaxPlayers = 2;
 
     public Transform PlayerOnePosition { get { return _playerOnePosition; } }
     public Transform PlayerTwoPosition { get { return _playerTwoPosition; } }
@@ -17,5 +22,33 @@ public class GameManager : MonoBehaviour
             Destroy(this);
         else
             Instance = this;
+    }
+
+    public override void OnNetworkSpawn()
+    {
+        base.OnNetworkSpawn();
+
+        if(IsServer)
+            NetworkManager.Singleton.OnClientConnectedCallback += OnClientConnectedCallback;
+    }
+
+    public override void OnNetworkDespawn()
+    {
+        base.OnNetworkDespawn();
+
+        if (IsServer)
+            NetworkManager.Singleton.OnClientConnectedCallback -= OnClientConnectedCallback;
+    }
+
+    private void OnClientConnectedCallback(ulong obj)
+    {
+        if (NetworkManager.Singleton.ConnectedClientsList.Count == MaxPlayers)
+            ShowButtonPanelClientRPC();
+    }
+
+    [ClientRpc]
+    private void ShowButtonPanelClientRPC()
+    {
+        _bettingButtonsPanel.SetActive(true);
     }
 }
