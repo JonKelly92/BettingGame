734eb7e33cc773e153e432af0c59a748a2b83144
JonKelly <jonkelly@protonmail.com>
2022-11-18 14:17:50 -0400

    Instantiating the chip stacks in rows and columns

--- BettingGame/Assets/Scripts/ChipManager.cs
+++ BettingGame/Assets/Scripts/ChipManager.cs
@@ -1,14 +1,33 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class ChipManager : NetworkBehaviour
 {
     [SerializeField] private ChipStack _chipStackPrefab;
+    [SerializeField] private Transform _chipSpawnRefrencePoint;
+
+    private const float MaxStacksPerRow = 5f;
+    private const float StackSpacing = 0.1f;
+
+    private List<ChipStack> _chipStackList;
+    private float _stackWidth;
+
 
     private void Awake()
     {
         EventManager.OnSpawnChips += OnSpawnChips;
+
+        _chipStackList = new List<ChipStack>();
+
+        var renderer = _chipStackPrefab.GetComponentInChildren<Renderer>();
+        if (renderer == null)
+            Debug.LogError("Could not find the Renderer component in the ChipStackPrefab");
+        else
+            _stackWidth = renderer.bounds.size.x;
     }
 
     public override void OnDestroy()
@@ -20,19 +39,60 @@ public class ChipManager : NetworkBehaviour
 
     private void OnSpawnChips()
     {
-        if (!IsOwner) return;
+        //if (!IsOwner) return;
 
-        float x = 0;
+        //float x = 0;
 
-        if (transform.position.x > 0)
-            x = 2f;
-        else
-            x = -2f;
+        //if (transform.position.x > 0)
+        //    x = 2f;
+        //else
+        //    x = -2f;
 
-        SpawnChipsServerRPC(new Vector3(x, 0, 0));
+        //SpawnChipsServerRPC(new Vector3(x, 0, 0));
+
+
+        for (int i = 0; i < 10; i++)
+        {
+            PrepareToSpawnChipStack();
+        }
 
     }
 
+    private void PrepareToSpawnChipStack()
+    {
+        if (!IsOwner) return;
+
+        if (_chipStackList.Count == 0)
+        {
+            // spawn first stack here -> _chipSpawnRefrencePoint
+            SpawnChipsServerRPC(_chipSpawnRefrencePoint.position);
+        }
+        else
+        {
+            if (_chipStackList.Count % MaxStacksPerRow != 0)
+            {
+                // we haven't got to the end of this row so lets add another stack
+                // get the last stack added to the list and create another stack next to it
+                Vector3 position = _chipStackList.Last().transform.position;
+                SpawnChipsServerRPC(new Vector3(
+                    position.x,
+                    position.y, 
+                    position.z - _stackWidth - StackSpacing));
+            }
+            else
+            {
+                // we have gotten to the end of the row
+                // get the amount of rows so far by dividing the List.Count by MaxStacksPerRow (represented by x)
+                // Then get the distance forward we should place the next stack -> (x * _stackWidth) + (x * StackSpacing) = distance to move
+                int numberOfRows = (int)(_chipStackList.Count / MaxStacksPerRow);
+                float distanceForward = (numberOfRows * _stackWidth) + (numberOfRows * StackSpacing);
+                SpawnChipsServerRPC(new Vector3(
+                    _chipSpawnRefrencePoint.position.x + distanceForward, 
+                    _chipSpawnRefrencePoint.position.y, 
+                    _chipSpawnRefrencePoint.position.z));
+            }
+        }
+    }
     [ServerRpc]
     private void SpawnChipsServerRPC(Vector3 position)
     {
@@ -49,6 +109,6 @@ public class ChipManager : NetworkBehaviour
     {
         var spawnedObject = Instantiate(_chipStackPrefab);
         spawnedObject.transform.position = position;
+        _chipStackList.Add(spawnedObject);
     }
-
 }
